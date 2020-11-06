663e95ffca344734dc058e98663ed9eac6942a58
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-11-07 03:01:09 +0530

    Update Linear-Search.c

--- Linear-Search.c
+++ Linear-Search.c
@@ -1,1 +1,32 @@
+#include<stdio.h>
 
+int main(){
+    int n,a,flag = -1;
+    int arr[n];
+   
+    printf("\n Enter the number of elements in the array :");
+    scanf("%d",&n);
+    
+    printf("\n Enter the elements of the array :");
+    for(int i = 0;i < n;i++) {
+        scanf("%d",&arr[i]);
+    }
+        
+    printf("\n The Array is : ");
+    for(int i = 0;i < n;i++) {
+        printf("%d ",arr[i]);
+    }
+        
+    printf("\n\n Enter the element to be searched : "); 
+    scanf("%d",&a);
+    
+    for(int i = 0;i < n;i++) {
+        if(arr[i] == a) {
+            flag = 1;
+            printf("\n The element %d is at the %dth position",a,i);
+        }
+    }
+    if(flag == -1) {
+            printf("\n The element %d  does not exist in the array",a);
+    }
+}
