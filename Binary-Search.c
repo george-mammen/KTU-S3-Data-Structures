9050f1a56257f005dc43c39f60f65ba1ab9fdd1a
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-11-07 02:38:29 +0530

    Update Binary-Search.c

--- Binary-Search.c
+++ Binary-Search.c
@@ -1,1 +1,45 @@
+#include<stdio.h>
 
+void main(){
+    int n ;
+    int arr[n];
+    int a;
+    int flag = -1;
+    
+    printf("\n Enter the number of elements in the array :");
+    scanf("%d",&n);
+    
+    printf("\n Enter the elements of the array :");
+    for(int i=0;i<n;i++) {
+        scanf("%d",&arr[i]);
+    }
+        
+    printf("\n The Unsorted array is : ");
+    for(int i=0;i<n;i++) {
+        printf("%d ",arr[i]);
+    }
+        
+    printf("\n The Sorted array is : ");    
+    for(int i = 0;i < n;i++) {
+        for(int j = i + 1;j < n;j++) {
+            if(arr[i]>arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        printf("%d ",arr[i]);
+    }
+    
+    printf("\n Enter the element to be searched : "); 
+    scanf("%d",&a);
+    for(int i = 0;i < n;i++) {
+        if(arr[i] == a) {
+            flag = 1;
+            printf("\n The element %d is at the %dth position",a,i);
+        }
+        }
+        if(flag == -1) {
+            printf("\n The element %d is does not exist in the array",a);
+    }
+}
