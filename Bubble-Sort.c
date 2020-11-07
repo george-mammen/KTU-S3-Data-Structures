11f26cacfc5d85e3c45e561c58d305ebaf02be49
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-11-07 12:07:21 +0530

    Update Bubble-Sort.c

--- Bubble-Sort.c
+++ Bubble-Sort.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
 
+void main(){
+    int n ;
+    int arr[n];
+    int a;
+    
+    printf("\n Enter the number of elements in the array :");
+    scanf("%d",&n);
+    
+    printf("\n Enter the elements of the array :");
+    for(int i=0;i<n;i++) {
+        scanf("%d",&arr[i]);
+    }
+        
+    printf("\n The Unsorted array is : ");
+    for(int i=0;i<n;i++) {
+        printf("%d ",arr[i]);
+    }
+        
+    printf("\n The Sorted array is : ");    
+    for(int i = 0;i < n;i++) {
+        for(int j = i + 1;j < n;j++) {
+            if(arr[i]>arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        printf("%d ",arr[i]);
+    }
