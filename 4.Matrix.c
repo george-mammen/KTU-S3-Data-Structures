826da1ff64b974b31c76d6c98eb2702711a61677
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-11-16 19:35:52 +0530

    Update 4.Matrix.c

--- 4.Matrix.c
+++ 4.Matrix.c
@@ -1,40 +1,67 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void main() {
-    int row,col,i,j,arr[10][10],tarr[10][10];
-    printf("\n Enter the number of rows :");
-    scanf("%d",&row);
-    printf("\n Enter the number of columns :");
-    scanf("%d", &col);
-    
-    printf("\n Enter the elements of the matrix :");
-        for(i = 0;i < row;i++) {
-            for(j = 0;j < col;j++) {
-                scanf("%d",&arr[i][j]);
+int i, j;
+
+int readarray(int a[10][10], int row, int col)
+{
+    printf("\n Enter the elements of the array :");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            scanf("%d", &a[i][j]);
         }
     }
+}
 
-    printf("\n The Matrix is : \n");
-    for (i = 0; i < row; i++) {
-        for (j = 0; j < col; j++) {
-            printf("%d  ", arr[i][j]);
+int printarray(int a[10][10], int row, int col)
+{
+    printf("\n The Matrix is: \n ");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            printf("%d  ", a[i][j]);
+        }
+        printf("\n ");
+    }
+}
+
+int tuplearray(int a[10][10], int row, int col){
+    printf("\n The matrix represented in tuple form :");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            if (a[i][j] != 0)
+            {
+                printf("(%d,%d,%d), ", i, j, a[i][j]);
+            }
+        }
+    }
+}
+
+int sumarray(int a[10][10],int b[10][10],int c[10][10], int row, int col) {
+    printf("\n The resultant matrix after addition : \n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            c[i][j] = a[i][j] + b[i][j];
+            printf(" %d  ", c[i][j]);
         }
         printf("\n");
     }
+}
 
-    printf("\n The matrix represented in tuple form :");
-        for(i = 0;i < row;i++) {
-            for(j = 0;j < col;j++) {
-                if(arr[i][j] != 0) {
-                    printf("(%d,%d,%d), ",i,j,arr[i][j]);
-                }
-            }
-        }
-
-    printf("\n \n The transform of the above matrix :");
-    for (i = 0; i < row; i++) {
-        for (j = 0; j < col; j++) {
-            tarr[i][j]=arr[j][i];
+    int transpose(int a[10][10],int b[10][10], int row, int col)
+{
+    printf("\n \n The transpose of the matrix : \n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            b[i][j] = a[j][i];
         }
     }
 
@@ -42,18 +69,35 @@ void main() {
     {
         for (j = 0; j < row; j++)
         {
-            printf("%d  ", tarr[i][j]);
-        } 
-        printf("\n");
-
-    }
-
-    printf("\n The transform of  matrix represented in tuple form : \n"); 
-    for (i = 0; i < col; i++) {
-        for (j = 0; j < row; j++) {
-            if (tarr[i][j] != 0) {
-                printf("(%d,%d,%d), ", i, j, tarr[i][j]);
-            }
+            printf("%d  ", b[i][j]);
         }
+        printf("\n");
     }
+}
+
+void main()
+{
+    int row, col, i, j, arr1[10][10], arr2[10][10], arr3[10][10], tarr1[10][10], tarr2[10][10], tarr3[10][10];
+    printf("\n Enter the number of rows :");
+    scanf("%d", &row);
+    printf("\n Enter the number of columns :");
+    scanf("%d", &col);
+
+    readarray(arr1,row,col);
+    printarray(arr1,row,col);
+    tuplearray(arr1,row,col);
+    transpose(arr1,tarr1,row,col);
+    tuplearray(tarr1, row, col);
+
+    printf("\n \n Second Matrix :");
+    readarray(arr2, row, col);
+    printarray(arr2, row, col);
+    tuplearray(arr2, row, col);
+    transpose(arr2, tarr2, row, col);
+    tuplearray(tarr2, row, col);
+
+    sumarray(arr1,arr2,arr3,row,col);
+    tuplearray(arr3, row, col);
+    transpose(arr3, tarr3, row, col);
+    tuplearray(tarr3, row, col);
 }
